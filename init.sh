09a97c15921465834cb7ba8e4fa0c74226d73c9d
sean-mephi <k.v.astahov@gmail.com>
2017-03-09 22:55:01 +0300

    fourth

--- init.sh
+++ init.sh
@@ -1,8 +1,5 @@
-#nginx conf
-sudo rm /etc/nginx/sites-enabled/default
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-
-#gunicorn conf
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/ask
-sudo /etc/init.d/gunicorn restart 
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo gunicorn -c /etc/gunicorn.d/hello.py hello:application
+sudo /etc/init.d/gunicorn restart
